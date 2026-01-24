41ea8838ff3a035d93b3db4c28103a4305b50855
Sangyun Kwon <ksu619@naver.com>
2026-01-25 03:57:43 +0900

    dynamics test

--- vehicleDynamics/src/main.cpp
+++ vehicleDynamics/src/main.cpp
@@ -5,25 +5,44 @@
 
 int main()
 {
-    Position center{};
-    center.set(0, 0, 3, -1.5708, 0, 0);
+    position* WheelPosition = new position[4];
+    WheelPosition[0] = {"",Vec3{1,1,0},Vec3{0,0,0}};
+    WheelPosition[1] = {"",Vec3{1,-1,0},Vec3{0,0,0}};
+    WheelPosition[2] = {"",Vec3{-1,1,0},Vec3{0,0,0}};
+    WheelPosition[3] = {"",Vec3{-1,-1,0},Vec3{0,0,0}};
 
-    Cylinder cyl(1.01, 0.5, center);
-    Box box(1.0, 0.99, 1.0, center);
-    Plane plane({0,0,1},{0,0,0});
+    Position VehiclePosition{Vec3{1,0,3},Vec3{0,0,0}};
 
-    Position p{};
-    p.set(0.0, 0.0, -0.5, 0, 0, 0);
-    Vec3 test = plane.closest(cyl);
-    cyl.checkcollsion(test);
-    box.checkcollsion(p.translation);
+    position Vehicle{"",VehiclePosition.translation,VehiclePosition.rotation};
+
+
+    position* GlobalWheelPosition = new position[4];
+
+
+    for(int i = 0 ; i < 4 ; i++)
+    {
+        GlobalWheelPosition[i] = Vehicle.ForwardKinematics(WheelPosition[i]);
+    }
     
 
-    std::cout << "cyl collision = "
-              << (cyl.collision ? "true" : "false")
-              << std::endl;
+    Cylinder Wheel[4] = {
+                            Cylinder(0.3,0.2,{GlobalWheelPosition[0].translation,GlobalWheelPosition[0].orientation}),
+                            Cylinder(0.3,0.2,{GlobalWheelPosition[1].translation,GlobalWheelPosition[1].orientation}),
+                            Cylinder(0.3,0.2,{GlobalWheelPosition[2].translation,GlobalWheelPosition[2].orientation}),
+                            Cylinder(0.3,0.2,{GlobalWheelPosition[3].translation,GlobalWheelPosition[3].orientation})
+                        };
+    
+    Plane plane(Vec3{0,0,1},Vec3{0,0,0});
 
-    std::cout << test.x << std::endl;
-    std::cout << test.y << std::endl;
-    std::cout << test.z << std::endl;
+
+    std::cout << plane.closest(Wheel[0]).x << std::endl;
+    std::cout << plane.closest(Wheel[0]).y << std::endl;
+    std::cout << plane.closest(Wheel[0]).z << std::endl;
+
+    Vec3 test = plane.closest(Wheel[0]);
+
+    std::cout <<  GlobalWheelPosition[0].InverseKinematics(position{"", test, Vec3{}} ).translation.x << std::endl;
+    std::cout <<  GlobalWheelPosition[0].InverseKinematics(position{"", test, Vec3{}} ).translation.y << std::endl;
+    std::cout <<  GlobalWheelPosition[0].InverseKinematics(position{"", test, Vec3{}} ).translation.z << std::endl;
+
 }
